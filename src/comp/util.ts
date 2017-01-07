5c1fe554036ae733a7bd9b1f7a6347b2f1ab862f
mamendex <mamendex@gmail.com>
2017-01-07 11:05:54 -0200

    comments added

--- src/comp/util.ts
+++ src/comp/util.ts
@@ -1,4 +1,13 @@
+/**
+ * Utils
+ * Generic functions
+ */
 
+ /**
+  * Sleep for @param ms microseconds (using setTimeout)
+  * usage:
+  *     sleep(500).then(...)
+  */
 function sleep(ms : number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
